90801386ad4a02853dd9bc48f397a5a8705dd8e9
Konstantin <kosvadmail@atomlc.com.ua>
2022-10-21 15:02:10 +0200

    Добавляет легкую валидацию форм и прикольный эффект при скролле

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,11 @@
 import { renderCarsCardsImage } from './index-render.js';
 
 renderCarsCardsImage();
+
+document.addEventListener("DOMContentLoaded", function () {
+  const elem = document.querySelector('.buy-form__bg');
+  console.log(elem.offsetTop + elem.parentNode.offsetTop);
+  document.addEventListener('scroll', () => {
+    elem.style.right = (-1 * (elem.offsetTop + elem.parentNode.offsetTop) * 0.3) + (0.3 * window.pageYOffset) + 'px';
+  });
+});
